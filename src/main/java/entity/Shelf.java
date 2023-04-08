fee634018d7dba70af54a0630b6209c3165c6f0d
Lorenzo <l.capelli14@studenti.unibg.it>
2023-04-08 12:02:05 +0200

    fixed shelf class

---
+++ src/main/java/entity/Shelf.java
@@ -0,0 +1,32 @@
+package entity;
+
+public class Shelf {
+	private Card[][] shelfLayout;
+	private int maxRaw = 6;
+	private int maxColumn = 5;
+	private int chosenColumn;
+	
+	public Shelf(int cardNumber, int chosenColumn) {
+		this.shelfLayout = new Card[6][5];
+		this.chosenColumn = chosenColumn;
+	}
+	public void PlaceOnShelf(int cardNumber, Card carta) {
+		int startPoint = 0;
+		int c = 0;
+		
+		while(shelfLayout[c][chosenColumn] != null) {
+			startPoint++;
+			c++;
+		}
+		
+		for(int i = 0; i < cardNumber; i++) {		
+			if((maxRaw - startPoint) >= cardNumber) {
+				for(int j = 0; j < cardNumber; j++) {
+					shelfLayout[startPoint+i][chosenColumn] = carta;
+				}
+			}else {
+				//error condition
+			}
+		}
+	}
+}
