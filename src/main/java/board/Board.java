832550666403bdd648098a5dfcb3b980a09a11b9
Lorenzo <l.capelli14@studenti.unibg.it>
2023-04-23 11:01:52 +0200

    edited place on shelf class, chosenFromBoard class

--- src/main/java/board/Board.java
+++ src/main/java/board/Board.java
@@ -200,12 +200,53 @@ public final class Board {
        return null;
    }
   
+   
+   
+   private int firstRaw;
+   private int firstCol;
+   private int oldRaw;
+   private int oldCol;
+   boolean first;
+   ArrayList<Card> chosenCards = new ArrayList<Card>();		            
+  
+ 	public void chosenFromBoard(int chosenRaw, int chosenCol) {		//deve ritornare l'array!!!!
+    	 
+    	 if(chosenCards.isEmpty()) {
+    		 first = true;
+    	 }else first = false;
+    	 
+    	 if(chosenCards.size() < 4) {
+        	 if(this.hasAFreeBorder(chosenRaw, chosenCol)) {
+        		 if(first) {
+        			 chosenCards.add(this.getCardAtCords(chosenRaw, chosenCol));
+        			 firstRaw = chosenRaw;
+        			 firstCol = chosenCol;
+        			 oldRaw = chosenRaw;
+        			 oldCol = chosenCol;
+        		 }
+        		 else {
+        			    if(chosenRaw == firstRaw) {
+        			    	if(chosenCol == oldCol + 1) {
+        			    		chosenCards.add(this.getCardAtCords(chosenRaw, chosenCol));
+        			    		oldRaw = chosenRaw;
+        	        			oldCol = chosenCol;
+        			    	}
+        			    }
+        			    if(chosenCol == firstCol) {
+        			    	if(chosenRaw == oldRaw + 1) {
+        			    		chosenCards.add(this.getCardAtCords(chosenRaw, chosenCol));
+        			    		oldRaw = chosenRaw;
+        	        			oldCol = chosenCol;
+        			    	}
+        			    }
+        		 }
+        	 }
+    	 }
+     }
     
-        
-           
-        
+    //alla fine remove placed card
 }
-        
+
         
         
       
--- src/main/java/entity/Shelf.java
+++ src/main/java/entity/Shelf.java
@@ -1,5 +1,7 @@
 package entity;
 
+import java.util.ArrayList;
+
 public class Shelf {
 	private Card[][] shelfLayout;
 	private int maxRaw = 6;
@@ -9,23 +11,31 @@ public class Shelf {
 		this.shelfLayout = new Card[6][5];
 		this.chosenColumn = chosenColumn;
 	}
-	public void PlaceOnShelf(int cardNumber, Card carta) {
+	
+	
+	
+	public void placeOnShelf(ArrayList<Card> chosenCards) {		
 		int startPoint = 0;
 		int c = 0;
+		int cardNumber = chosenCards.size();
 		
 		while(shelfLayout[c][chosenColumn] != null) {
 			startPoint++;
 			c++;
 		}
-		
-		for(int i = 0; i < cardNumber; i++) {		
-			if((maxRaw - startPoint) >= cardNumber) {
-				for(int j = 0; j < cardNumber; j++) {
-					shelfLayout[startPoint+i][chosenColumn] = carta;
+			
+			if((maxRaw - startPoint) >= cardNumber) {	
+				for(int i = 0; i < cardNumber; i++) {
+					shelfLayout[startPoint+i][chosenColumn] = chosenCards.get(i);
 				}
-			}else {
+				for(int i = 0; i < chosenCards.size(); i++) {
+					chosenCards.remove(i);
+				}
+			}
+			else {
 				//error condition
 			}
 		}
 	}
-}
+
+
