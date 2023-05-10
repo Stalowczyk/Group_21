e2eb40bfc1b7e6f794ba2aa1347bc2d5fbc70f3d
Lorenzo <l.capelli14@studenti.unibg.it>
2023-05-10 08:46:05 +0200

    .

--- src/main/java/entity/PersonalGoalsCards.java
+++ src/main/java/entity/PersonalGoalsCards.java
@@ -10,60 +10,61 @@ public class PersonalGoalsCards {
 	int number;
 	final int NumberOfRow = 6;
 	final int numberOfCol = 5;
-	
-	int[] personalGoal; 
-	int[][] personalGoalCardsLayout; 
+
+	int[] personalGoal;
+	int[][] personalGoalCardsLayout;
 	public ArrayList<Card> currentGoalCardLayout = new ArrayList<>();
-	
+
 	private int numberOfPlayers;
-	
+
 	ArrayList<Card> spaceIn;
-	
+
 	GamePanel gp;
-	
-	public PersonalGoalsCards(int numberOfPlayers, GamePanel gp) {		//a seconda di quanti giocatori ci sono so quante goalsCards generare
+
+	public PersonalGoalsCards(int numberOfPlayers, GamePanel gp) { // a seconda di quanti giocatori ci sono so quante
+																	// goalsCards generare
 		this.spaceIn = new ArrayList<>();
 		this.numberOfPlayers = numberOfPlayers;
 		this.gp = gp;
 	}
-	
-	public int[][] getPersonalGoalCardsLayout(){
+
+	public int[][] getPersonalGoalCardsLayout() {
 		return this.personalGoalCardsLayout;
 	}
-	
-	public void randomizeSixSpace() {		//genera il numero della casella dove andrà il goal
+
+	public void randomizeSixSpace() { // genera il numero della casella dove andrà il goal
 		personalGoal = new int[6];
 		Random random = new Random();
-        for(int i = 0; i <= numberOfSpace; i++) {
-        	number = random.nextInt(31);
-        	personalGoal[i] = number;
-        }
-        
-        personalGoalCardsLayout = new int[][] {{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0}};
-		
-        for(int i = 0; i < numberOfSpace; i++) {
-			personalGoalCardsLayout[personalGoal[i] / 5][(personalGoal[i] % 5) - 1] = 1; //da sistemare
+		for (int i = 0; i <= numberOfSpace; i++) {
+			number = random.nextInt(31);
+			personalGoal[i] = number;
+		}
+
+		personalGoalCardsLayout = new int[][] { { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 } };
+
+		for (int i = 0; i < numberOfSpace; i++) {
+			personalGoalCardsLayout[personalGoal[i] / 5][(personalGoal[i] % 5) - 1] = 1;
 		}
 	}
-	
+
 	public void placeCardsOnSpaces() {
-		for(int i = 0; i <= 6; i++) {
-			for(int j = 0; j <= 5; j++) {
-				if(personalGoalCardsLayout[i][j] == 1) {
-					PlacedCard card = new PlacedCard(spaceIn.get(i).getCardType(), i, j, gp);	//genero una carta casuale
+		for (int i = 0; i <= 6; i++) {
+			for (int j = 0; j <= 5; j++) {
+				if (personalGoalCardsLayout[i][j] == 1) {
+					PlacedCard card = new PlacedCard(spaceIn.get(i).getCardType(), i, j, gp);
 					currentGoalCardLayout.add(card);
 				}
 			}
-		}	
-	}
-	 
-	public void AddAllTiles() {
-		for(int i = 0; i <= 6; i++) {
-			for(CardType s : CardType.values()){
-                Card c = new Card(s);
-				spaceIn.add(c);
-            }
 		}
 	}
-	
+
+	public void AddAllTiles() {
+		for (int i = 0; i <= 6; i++) {
+			for (CardType s : CardType.values()) {
+				Card c = new Card(s);
+				spaceIn.add(c);
+			}
+		}
+	}
 }
--- src/main/java/entity/Shelf.java
+++ src/main/java/entity/Shelf.java
@@ -6,47 +6,45 @@ public class Shelf {
 	private Card[][] shelfLayout;
 	private int maxRaw = 6;
 	private int chosenColumn;
-	
+
 	public Shelf(int cardNumber, int chosenColumn) {
 		this.shelfLayout = new Card[6][5];
 		this.chosenColumn = chosenColumn;
 	}
-	
-	
-	
-	public void placeOnShelf(ArrayList<Card> chosenCards) {		
+
+	public void placeOnShelf(ArrayList<Card> chosenCards) {
 		int startPoint = 0;
 		int c = 0;
 		int cardNumber = chosenCards.size();
-		
-		while(shelfLayout[c][chosenColumn] != null) {
+
+		while (shelfLayout[c][chosenColumn] != null) {
 			startPoint++;
 			c++;
 		}
-			
-			if((maxRaw - startPoint) >= cardNumber) {	
-				for(int i = 0; i < cardNumber; i++) {
-					shelfLayout[startPoint+i][chosenColumn] = chosenCards.get(i);
-				}
-				for(int i = 0; i < chosenCards.size(); i++) {
-					chosenCards.remove(i);
-				}
+
+		if ((maxRaw - startPoint) >= cardNumber) {
+			for (int i = 0; i < cardNumber; i++) {
+				shelfLayout[startPoint + i][chosenColumn] = chosenCards.get(i);
 			}
-			else {
-				//error condition
+			for (int i = 0; i < chosenCards.size(); i++) {
+				chosenCards.remove(i);
 			}
+		} else {
+			// error condition
 		}
-	public boolean checkCorners() {                            //Sistema di controllo angoli della shelf
-		CardType type = null;                                  //Controlla se tutti gli angoli hanno lo stesso cardType, serve per obbiettivo collettivo
-		if (this.shelfLayout[0][0] != null) {                  // Filo
+	}
+
+	public boolean checkCorners() { 			// Sistema di controllo angoli della shelf
+		CardType type = null; 					// Controlla se tutti gli angoli hanno lo stesso cardType, serve per obbiettivo
+												// collettivo
+		if (this.shelfLayout[0][0] != null) { 	// Filo
 			type = this.shelfLayout[0][0].getCardType();
 		}
-		if (type != null && type == this.shelfLayout[0][4].getCardType()
-				&& type == this.shelfLayout[5][0].getCardType()
+		if (type != null && type == this.shelfLayout[0][4].getCardType() && type == this.shelfLayout[5][0].getCardType()
 				&& type == this.shelfLayout[5][4].getCardType()) {
 			return true;
 		}
-		return false;}
+		return false;
 	}
-
+}
 
