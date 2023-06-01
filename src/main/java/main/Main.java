0229a664a5c13d818f04a165a226094747dbabd3
Filooh <filippo.panighetti@gmail.com>
2023-06-01 06:36:31 +0200

    Fixed Image Icon
    Adesso il gioco ha correttamente il logo cranio

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -9,12 +9,12 @@ import entity.Bag;
 import entity.CommonGoals;
 import entity.PersonalGoalsCards;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridBagLayout;
+import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Scanner;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -26,16 +26,22 @@ import javax.swing.JOptionPane;
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //BUG SISTEMARE DIMENSIONI IN BASE ALLA RISOLUZIONE DEL PC 
         
         JFrame window = new JFrame();
         window.setLayout(new GridBagLayout());
         GamePanel gamePanel = new GamePanel();
 
+
+        ClassLoader classLoader = Main.class.getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("logo-cranio.png");
+        assert inputStream != null;
+        Image image = ImageIO.read(inputStream);
+        ImageIcon imageIcon = new ImageIcon(image);
+        Image frameIcon = imageIcon.getImage();
+        window.setIconImage(frameIcon);
         
-        ImageIcon image = new ImageIcon("logo-cranio.png");
-        window.setIconImage(image.getImage());
         window.setTitle("  My Shelfie - Group 21");
         
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
